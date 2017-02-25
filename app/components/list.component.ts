5fa15ef58fa7915e315f078c854b668c205bdf45
Nicolai Guzun <nicolai.guzun@iquestgroup.com>
2017-02-25 14:50:42 +0200

    Create model: car.ts Read data from separate data.ts file

--- app/components/list.component.ts
+++ app/components/list.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CARS } from '../data/data';
+import { Car } from '../models/car.model';
 
-const CARS = [
-    "Mercedes B180",
-    "Mercedes B200",
-    "Mercedes C200",
-    "Mercedes E220",
-    "Mercedes S180",
-    "BMW 520d"
-];
 
 @Component({
     selector: 'list-component',
@@ -15,7 +9,7 @@ const CARS = [
 })
 export class ListComponent implements OnInit {
 
-    public cars: Array<string>;
+    public cars: Array<Car>;
 
     ngOnInit() {
         this.cars = CARS;
---
+++ app/models/car.model.ts
@@ -0,0 +1,20 @@
+export class Car {
+    id: number;
+    brandName: string;
+    model: string;
+    description: string;
+    image: string;
+    engineCap: number;
+
+    constructor(id: number, brandName: string, model: string, description: string,  image: string, engineCap: number) {
+        this.id = id;
+        this.brandName = brandName;
+        this.model = model;
+        this.description = description;
+        this.image = image;
+        this.engineCap = engineCap;
+    }
+    public carFullName() {
+        return `${this.brandName} ${this.model}`;
+    }
+}
