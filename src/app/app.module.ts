8cbff4c8843b7ae61a6a48277c40b5092429c01a
Marius-Valentin Axinte <marius-valentin.axinte@teamaol.com>
2017-04-03 03:10:53 +0300

    Small code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,6 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import { ListComponent } from "./components/list.component";
-// import { DetailComponent } from "./components/detail.component";
-// import { LikeComponent } from "./components/like.component";
-// import { ZoomImageDirective } from './directives/zoomImage.directive';
-// import { Cm3ToLiters } from './pipes/cm3ToLiters.pipes';
 import { SharedModule } from './shared/shared.module'
 import { CarsModule } from './cars/cars.module'
 
@@ -17,14 +12,7 @@ import { CarsModule } from './cars/cars.module'
     SharedModule,
     CarsModule
   ],
-  declarations: [
-                    AppComponent,
-                    // ListComponent,
-                    // DetailComponent,
-                    // LikeComponent,
-                    // ZoomImageDirective,
-                    // Cm3ToLiters
-  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
