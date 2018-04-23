59b5d15e5524fee7c99b94c755a095e7cda33d2e
Marius A <marius.axinte@iquestgroup.com>
2018-04-23 14:06:00 +0300

    Searchable field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
-import { CarsModule } from './cars/cars.module'
+import { CarsModule } from './cars/cars.module';
+import { ObservablesComponent } from './observables/observables.component'
 
 
 @NgModule({
@@ -13,7 +14,7 @@ import { CarsModule } from './cars/cars.module'
     SharedModule,
     CarsModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ObservablesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/observables/observables.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observables',
+  templateUrl: './observables.component.html',
+  styleUrls: ['./observables.component.css']
+})
+export class ObservablesComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
