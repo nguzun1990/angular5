1e9f0fe91c347b615660e881dd8aa29be5f1bd71
Marius A <marius.axinte@iquestgroup.com>
2018-04-24 01:55:36 +0300

    Added HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module'
 import { SharedModule } from './shared/shared.module'
@@ -12,7 +12,7 @@ import { CarsModule } from './cars/cars.module'
 @NgModule({
   imports:      [ 
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     DashboardModule,
     SharedModule,
     CarsModule,
