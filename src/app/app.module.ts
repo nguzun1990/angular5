de095deccbf0daa1b548b4a256947871d9ca0c20
Marius A <marius.axinte@iquestgroup.com>
2018-04-24 01:49:45 +0300

    Added HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
 import { CarsModule } from './cars/cars.module';
@@ -8,13 +8,13 @@ import { ObservablesComponent } from './observables/observables.component'
 
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     SharedModule,
     CarsModule
   ],
-  declarations: [ AppComponent, ObservablesComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, ObservablesComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core'
-import { Http } from '@angular/http'
+import { HttpClient } from '@angular/common/http'
 
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
@@ -9,28 +9,28 @@ import { Car } from './car.model';
 @Injectable()
 export class CarsService {
 
-    constructor(
-        protected http: Http,
-        @Inject('API_URL') private APIUrl: string
-    ) {
+  constructor(
+    protected http: HttpClient,
+    @Inject('API_URL') private APIUrl: string
+  ) {
 
-    }
+  }
 
-    getCars(): Observable<Array<Car>> {
-        return this.http.get(this.APIUrl + '/cars')
-            .map(response => response.json().map(this.hydrateCar))
-    }
+  getCars(): Observable<Array<Car>> {
+    return this.http.get<Array<Car>>(this.APIUrl + '/cars')
+      .map(response => response.map(this.hydrateCar));
+  }
 
-    hydrateCar(data: any) {
-        return new Car(
-            data.id || null, 
-            data.brandName || null,
-            data.model || null,
-            data.description || null,
-            data.image || null,
-            data.engineCap || null,
-            data.likes || null, 
-            data.liked || null
-        );
-    }
+  hydrateCar(data: any) {
+    return new Car(
+      data.id || null,
+      data.brandName || null,
+      data.model || null,
+      data.description || null,
+      data.image || null,
+      data.engineCap || null,
+      data.likes || null,
+      data.liked || null
+    );
+  }
 }
